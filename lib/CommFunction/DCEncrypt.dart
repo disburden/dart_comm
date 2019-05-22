4ccf10b1ec426880221f9bbde6cc5a4af5291a11
disburden <disburden.gamil.com>
2019-05-22 11:33:01 +0800

    back encrypt some function name

--- lib/CommFunction/DCEncrypt.dart
+++ lib/CommFunction/DCEncrypt.dart
@@ -8,7 +8,7 @@ import 'package:flutter_des/flutter_des.dart';
 class DCEncrypt{
 
 	/// Aes加解密
-	static String DCEncryptUseAes(String originText,String keyStr) {
+	static String encryptUseAes(String originText,String keyStr) {
 		final key = Key.fromUtf8(keyStr);
 		final iv = IV.fromLength(16);
 		final encrypter = Encrypter(AES(key, iv,mode: AESMode.ecb));
@@ -18,7 +18,7 @@ class DCEncrypt{
 		return encrypted.base64;
 	}
 
-	static String DCDecryptUseAes(String encryptedStr,String keyStr){
+	static String decryptUseAes(String encryptedStr,String keyStr){
 		final key = Key.fromUtf8(keyStr);
 		final iv = IV.fromLength(16);
 		final encrypter = Encrypter(AES(key, iv,mode: AESMode.ecb));
@@ -30,44 +30,44 @@ class DCEncrypt{
 	}
 
 	/// 3des加解密(注意:对中文支持有点问题)
-	static String DCEncryptUse3des(String originText,String keyStr) {
+	static String encryptUse3des(String originText,String keyStr) {
 		var blockCipher = new BlockCipher(new DESEngine(), keyStr);
 		return blockCipher.encodeB64(originText);
 	}
 
-	static String DCDecryptUse3des(String encryptedStr,String keyStr){
+	static String decryptUse3des(String encryptedStr,String keyStr){
 		var blockCipher = new BlockCipher(new DESEngine(), keyStr);
 		return blockCipher.decodeB64(encryptedStr);
 	}
 
 	/// md5
-	static String DCEncryptUseMd5(String data) {
+	static String encryptUseMd5(String data) {
 		List<int> intList = utf8.encode(data);
 		var result = md5.convert(intList);
 		return result.toString();
 	}
 
 	/// base64加解密
-	static String DCEncodeUseBase64(String data){
+	static String encodeUseBase64(String data){
 		Uint8List u8Str = Uint8List.fromList(data.codeUnits);
 		String base64Str = base64Encode(u8Str);
 		return base64Str;
 	}
 
-	static String DCDecodeUseBase64(String data){
+	static String decodeUseBase64(String data){
 		Uint8List u8Str =  base64Decode(data);
 		String originStr = String.fromCharCodes(u8Str);
 		return originStr;
 	}
 	
 	/// des加密?
-	static Future<String> DCEncodeUseDes(String originText,String keyStr,String iv) async{
+	static Future<String> encodeUseDes(String originText,String keyStr,String iv) async{
 		var encryptBase64 = await FlutterDes.encryptToBase64(originText, keyStr, iv: iv);
 		return encryptBase64;
 	}
 	
 	/// des解密?
-	static Future<String> DCDecodeUseDes(String encryptedStr,String keyStr,String iv) async{
+	static Future<String> decodeUseDes(String encryptedStr,String keyStr,String iv) async{
 		var originStr = await FlutterDes.decryptFromBase64(encryptedStr, keyStr, iv: iv);
 		return originStr;
 	}
